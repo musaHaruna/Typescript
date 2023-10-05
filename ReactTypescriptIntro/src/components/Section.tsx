44e6fcaf0fdff70fef2d4dc01371d9e4bea4a100
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-10-04 18:33:54 -0700

    ReactTypescript-intro-final-code

---
+++ ReactTypescriptIntro/src/components/Section.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react"
+
+type SectionProps = {
+    title?: string,
+    children: ReactNode
+}
+
+
+
+
+export const Section = ({ children, title = "My Subheading" }: SectionProps) => {
+    return (
+        <section>
+            <h2>{title}</h2>
+            <p>{children}</p>
+        </section>
+    )
+}
